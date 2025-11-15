fc14c7862f6b7ff7d62bc1e6748f1b00404c79f1
fund-dashboard <ktjunior@seznam.cz>
2025-11-15 11:28:08 +0100

    Update generate.py

--- generate.py
+++ generate.py
@@ -3,9 +3,9 @@ import requests
 from datetime import datetime
 import json
 
-SHARES = 0.3
+SHARES = 3.0
 PURCHASE_DATE = "2025-11-14"
-PURCHASE_PRICE = 141.90  # EUR
+PURCHASE_PRICE = 141.80  # EUR
 TEMPLATE = "template.html"
 OUTPUT = "index.html"
 
